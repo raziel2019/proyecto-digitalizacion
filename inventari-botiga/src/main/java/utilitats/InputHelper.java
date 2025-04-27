ee405a1e727852eecc1637f2389400f9bbd0083f
macbookpro14m2 <hraziel487@gmail.com>
2025-04-27 22:23:16 +0200

    Creacion de Helper para todo el programa

--- inventari-botiga/src/main/java/utilitats/InputHelper.java
+++ inventari-botiga/src/main/java/utilitats/InputHelper.java
@@ -1,5 +1,69 @@
 package utilitats;
+import java.util.Scanner;
 
 public class InputHelper {
 
-}
+	private static final Scanner scanner = new Scanner(System.in);
+	
+	public static int readInteger() {
+		int numero = 0;
+		boolean valido = false;
+	
+		while(!valido) {
+			try{
+			System.out.print("Introduce un número entero: ");
+			numero = Integer.parseInt(scanner.nextLine());
+			valido = true;
+		}catch(NumberFormatException error) {
+			System.out.print("Error: no es un número entero no valido");
+		}		
+	}
+		return numero;
+	}
+	
+	public static int readPositiveInteger() {
+		int numero = -1;
+		while(numero < 0) {
+			numero = readInteger();
+			if(numero < 0) {
+				System.out.println("Error introduce un numero positivo");
+			}
+		}
+		return numero;
+		}
+	
+	public static double readDecimal() {
+		double numero = 0.0;
+		boolean valido = false;
+		
+		while(!valido) {
+			try {
+				System.out.println("Introduce un numero decimal");
+				numero = Double.parseDouble(scanner.nextLine());
+				valido = true;
+			}catch(NumberFormatException error) {
+				System.out.print("Haz introducido un numero que no es");
+				
+			}
+		}
+		
+		return numero;
+		
+	}
+	
+	public static String readString() {
+		String texto = "";
+		
+		while(texto.isBlank()) {
+			System.out.print("Introduce un texto: ");
+			texto = scanner.nextLine().trim();
+			if(texto.isBlank()){
+				System.out.print("No puede estar vacio");
+			}	
+		}
+		return texto;
+	}
+		
+	}
+	
+
