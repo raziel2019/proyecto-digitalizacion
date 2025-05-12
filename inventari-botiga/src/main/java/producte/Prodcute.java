6faf0bc42081d76160c5e54939e522f4d70bf7c6
Oscarrodriguez2 <orodrigue2@institutjaumehuguet.cat>
2025-05-12 19:06:17 +0200

    Update Prodcute.java marcoscar

--- inventari-botiga/src/main/java/producte/Prodcute.java
+++ inventari-botiga/src/main/java/producte/Prodcute.java
@@ -1,42 +1,47 @@
 package producte;
 
 public class Producte {
-    private String nom;
-    private double preu;
+    private String nombre;
+    private double precio;
     private int stock;
 
-    public Producte(String nom, double preu, int stock) {
-        this.nom = nom;
-        this.preu = preu;
+    public Producte(String nombre, double precio, int stock) {
+        this.nombre = nombre;
+        this.precio = precio;
         this.stock = stock;
     }
 
-    public String getNom() {
-        return nom;
+    public String getNombre() {
+        return nombre;
     }
 
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public double getPreu() {
-        return preu;
-    }
-
-    public void setPreu(double preu) {
-        this.preu = preu;
+    public double getPrecio() {
+        return precio;
     }
 
     public int getStock() {
         return stock;
     }
 
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
     public void setStock(int stock) {
         this.stock = stock;
     }
 
+    public boolean descontarStock(int cantidad) {
+        if (cantidad <= stock) {
+            stock -= cantidad;
+            return true;
+        }
+        return false;
+    }
+
     @Override
     public String toString() {
-        return "Producte: " + nom + ", Preu: " + preu + "€, Stock: " + stock;
+        return String.format("%s – Precio: %.2f €, Stock: %d", nombre, precio, stock);
     }
 }
+
